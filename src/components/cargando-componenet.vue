5b02ae62a849cabe9aa6e1e7ed059c0c69b1ae62
Crespo-Vasquez Edison-Enrique <edison.crespo.ext@inetum.com>
2024-10-18 13:55:03 +0200

    Nuevo componente cargando

---
+++ src/components/cargando-componenet.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "cargando-component"
+}
+
+</script>
+<template>
+  <div class="mensajeCargando">
+    <h3>Cambiando de color....</h3>
+  </div>
+</template>
+<style>
+
+.mensajeCargando {
+    background-color: blue;
+    color: white;
+}
+</style>
--- src/components/panelInformativo-component.vue
+++ src/components/panelInformativo-component.vue
@@ -1,10 +1,11 @@
 <script>
+import cargandoComponenet from "./cargando-componenet.vue";
 export default {
   name: "panelInformativo-component",
+  components: cargandoComponenet,
   methods: {
     cambiarColorFondo() {
-      alert("cambiando...");
-
+      
       var estilos = window.getComputedStyle(document.body);
       var colorFondoActual = estilos.backgroundColor;
       var colorTextoActual = estilos.color;
@@ -22,8 +23,35 @@ export default {
         document.getElementById("modoNocturnoDia").innerText = "☽";
       }
     },
-    redirectLinkedin(){
-     window.open("www.linkedin.com/in/edisonenriquedev", "LinkedinEdison")
+    redirectLinkedin() {
+      window.open("https://www.linkedin.com/in/edisonenriquedev", "_blank");
+    },
+    mostrarManga() {
+      const data = null;
+
+      const xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+          console.log(this.responseText);
+        }
+      });
+
+      xhr.open(
+        "GET",
+        "https://mangaverse-api.p.rapidapi.com/manga/fetch?page=1&genres=Harem%2CFantasy&nsfw=true&type=all"
+      );
+      xhr.setRequestHeader(
+        "x-rapidapi-key",
+        "dfa44317abmshf4032bd655040a8p1d862djsn96130dd57b9f"
+      );
+      xhr.setRequestHeader("x-rapidapi-host", "mangaverse-api.p.rapidapi.com");
+
+      xhr.send(data);
+    },
+    toggleCargandoComponent() {
+      this.mostrarCargandoComponent = !this.mostrarCargandoComponent;
     }
   },
 };
@@ -32,8 +60,11 @@ export default {
 <template>
   <div class="panel_informativo">
     <button @click="cambiarColorFondo" id="modoNocturnoDia">☽</button>
-    <br><br>
+    <br /><br />
     <button @click="redirectLinkedin">In</button>
+    <br /><br />
+    <button @click="mostrarManga">M</button>
+    <cargandoComponenet v-if="mostrarCargandoComponent"></cargandoComponenet>
   </div>
 </template>
 
